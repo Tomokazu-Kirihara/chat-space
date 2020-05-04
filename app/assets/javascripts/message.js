f7a28f768b70d9d528465f5e256914e6f6e84a2f
Tomokazu-Kirihara <miw55kzts@icloud.com>
2020-05-04 18:16:23 +0900

    user function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -87,7 +87,6 @@ $(function(){
     .fail(function() {
       alert('error');
     });
-    
   }
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
